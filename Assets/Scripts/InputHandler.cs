9cd3ef8d212ebf5978d18983fcb9f04435875a6a
Vicion4 <victor_ion02@hotmail.com>
2025-12-17 21:59:52 +0200

    component1

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -4,16 +4,16 @@ using UnityEngine.InputSystem;
 public class InputHandler : MonoBehaviour
 {
     public PlayerController CharacterController;
-    private InputAction _moveAction, _lookAction, _jumpAction;
+    private InputAction _moveAction, _lookAction;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _moveAction = InputSystem.actions.FindAction("Move");
         _lookAction = InputSystem.actions.FindAction("Look");
-        _jumpAction = InputSystem.actions.FindAction("Jump");
+       
 
-        _jumpAction.performed += OnJumpPerformed;
+       
 
         Cursor.visible = false;
 
@@ -25,14 +25,5 @@ public class InputHandler : MonoBehaviour
     {
         Vector2 movementVector = _moveAction.ReadValue<Vector2>();
         CharacterController.Move(movementVector);
-
-        Vector2 lookVector = _lookAction.ReadValue<Vector2>();
-        CharacterController.Rotate(lookVector);
     }
-
-    private void OnJumpPerformed(InputAction.CallbackContext context)
-    {
-        CharacterController.Jump();
-    }
-    
 }
